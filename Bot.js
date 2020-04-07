d0b428a0aad4666464f145ab610809f5780ea4e6
amorimchan <cucaratcha@icloud.com>
2020-04-07 12:57:53 -0300

    voicechat do ratatui

--- Bot.js
+++ Bot.js
@@ -2,7 +2,7 @@
 
 const discord = require("discord.js");
 const client = new discord.Client();
-client.login("Njk1Mzc4Mjg2NjQ1MDg0MjI0.XouLyw.KjHKGcp5i22hLaNGXSblC5lbAd0"/*bot token*/);
+client.login("XXXXXXXXXXXXXX"/*bot token*/);
 const Youtube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
@@ -44,8 +44,8 @@ client.on("message", msg =>
       msg.channel.send("pseudo dono e melhor veterano");
     }
 
-    else if (msg.content.toLowerCase().includes("poli")){
-      msg.channel.send("clb Poli");
+    else if (msg.content.toLowerCase().includes("idalgo")){
+      msg.channel.send("mais conhecido nas boca de fumo por tom moletom");
     }
     
 
@@ -87,6 +87,25 @@ client.on("message", msg =>
        else {
         msg.reply('entra num canal primeiro parsa');
       }}
+
+      else if (msg.content === 'ratatui') {
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (msg.member.voice.channel) {
+          const voicechannel = msg.member.voice.channel;
+          voicechannel.join().then(connection => 
+            {
+                const dispatcher = connection.play(
+                  ytdl("https://www.youtube.com/watch?v=p_hdmt4vpBo", { // pass the url to .ytdl()
+                    quality: 'highestaudio',
+                    // download part of the song before playing it
+                    // helps reduces stuttering
+                    highWaterMark: 1024 * 1024 * 10
+                  }));
+            }).catch(err => {console.log(err); VoiceChannel.leave();});
+          }
+         else {
+          msg.reply('entra num canal primeiro parsa');
+        }}
     //else if(msg.content == "")
     /*seus else if vem aqui*/
 });
